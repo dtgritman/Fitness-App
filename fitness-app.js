fe273924b8679c57ffbd1295c7ac04a590a49432
dtgritman <dtgritman@gmail.com>
2021-09-20 14:25:29 -0400

    changed main app settings to work with heroku

--- fitness-app.js
+++ fitness-app.js
@@ -1,15 +1,12 @@
-
 const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 80;
+const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html');
   res.end('This is a test');
 });
 
 server.listen(port, hostname, () => {
-  console.log("Server running at http://${hostname}:${port}/");
+  console.log("Server running on port: ${port}/");
 });
