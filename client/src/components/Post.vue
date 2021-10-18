38635dd88696b52483c53154604d7cc7541ea209
Dustin Gritman <gritmand1@hawkmail.newpaltz.edu>
2021-10-17 22:52:02 -0400

    Feed now uses temporary data

---
+++ client/src/components/Post.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="card">
+        <article class="media">
+            <figure class="media-left">
+                <p class="image is-64x64">
+                    <img src="/imgs/default-avatar.png" />
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <p>
+                        <strong>
+                            {{ post.userHandle }}
+                        </strong>
+                        <br />
+                        <img :src="post.src">
+                        {{ post.caption }}
+                        <br />
+                        <small>{{ post.likes }} Likes </small>
+                        <br />
+                        <small>
+                            <a>Like</a> · <a>Reply</a> ·
+                            {{ post.time }}
+                        </small>
+                    </p>
+                </div>
+                <!--
+                <article
+                    class="media"
+                    v-for="(postComment, index) in post.comments"
+                    :key="index"
+                >
+                    <figure class="media-left">
+                        <p class="image is-48x48">
+                            <img src="/imgs/default-avatar.png" />
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div class="content">
+                            <p>
+                                <strong>
+                                    {{ postComment.firstname }}
+                                    {{ postComment.lastname }}
+                                </strong>
+                                <br />
+                                {{ postComment.comment }}
+                                <br />
+                                <small> {{ postComment.likes }} Likes </small>
+                                <br />
+                                <small>
+                                    <a>Like</a> ·
+                                    {{ postComment.datetime }}
+                                </small>
+                            </p>
+                        </div>
+                    </div>
+                </article>
+                -->
+            </div>
+        </article>
+        <!--
+        <article class="media">
+            <figure class="media-left">
+                <p class="image is-64x64">
+                    <img src="/imgs/default-avatar.png" />
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="field">
+                    <p class="control">
+                        <textarea
+                            class="textarea"
+                            placeholder="Write a comment..."
+                        ></textarea>
+                    </p>
+                </div>
+                <div class="field">
+                    <p class="control">
+                        <button class="button">Post comment</button>
+                    </p>
+                </div>
+                <br />
+            </div>
+        </article>
+        -->
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        post: Object
+    }
+};
+</script>
+
+<style>
+</style>
--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -1,103 +1,24 @@
 <template>
     <section class="section">
         <div v-for="(post, postid) in posts" :key="postid">
-            <div class="card">
-                <article class="media">
-                    <figure class="media-left">
-                        <p class="image is-64x64">
-                            <img src="/imgs/default-avatar.png" />
-                        </p>
-                    </figure>
-                    <div class="media-content">
-                        <div class="content">
-                            <p>
-                                <strong>
-                                    {{ post.firstname }}
-                                    {{ post.lastname }}
-                                </strong>
-                                <br />
-                                {{ post.info }}
-                                <br />
-                                <small>{{ post.likes }} Likes </small>
-                                <br />
-                                <small>
-                                    <a>Like</a> · <a>Reply</a> ·
-                                    {{ post.datetime }}
-                                </small>
-                            </p>
-                        </div>
-
-                        <article
-                            class="media"
-                            v-for="(postComment, index) in post.comments"
-                            :key="index"
-                        >
-                            <figure class="media-left">
-                                <p class="image is-48x48">
-                                    <img src="/imgs/default-avatar.png" />
-                                </p>
-                            </figure>
-                            <div class="media-content">
-                                <div class="content">
-                                    <p>
-                                        <strong>
-                                            {{ postComment.firstname }}
-                                            {{ postComment.lastname }}
-                                        </strong>
-                                        <br />
-                                        {{ postComment.comment }}
-                                        <br />
-                                        <small>
-                                            {{ postComment.likes }} Likes
-                                        </small>
-                                        <br />
-                                        <small>
-                                            <a>Like</a> ·
-                                            {{ postComment.datetime }}
-                                        </small>
-                                    </p>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
-                </article>
-                <article class="media">
-                    <figure class="media-left">
-                        <p class="image is-64x64">
-                            <img src="/imgs/default-avatar.png" />
-                        </p>
-                    </figure>
-                    <div class="media-content">
-                        <div class="field">
-                            <p class="control">
-                                <textarea
-                                    class="textarea"
-                                    placeholder="Write a comment..."
-                                ></textarea>
-                            </p>
-                        </div>
-                        <div class="field">
-                            <p class="control">
-                                <button class="button">Post comment</button>
-                            </p>
-                        </div>
-                        <br />
-                    </div>
-                </article>
-            </div>
+            <Post :post="post" />
             <br />
         </div>
     </section>
 </template>
 
 <script>
-import session from "../services/session"
-//import posts from "../services/posts";
+import Post from "../components/Post.vue";
+import session from "../services/session";
+import { getWall } from "../services/posts";
 
 export default {
     name: "Feed",
+    components: { Post },
     data: () => ({
         session,
+        posts: getWall(session.user.handle),
+        /*
         posts: {
             182: {
                 userid: 9309,
@@ -144,6 +65,7 @@ export default {
                 ],
             },
         },
+        */
     }),
 };
 </script>
