2855b2b86818a3d82114aab8b1ea14034013f055
dtgritman <dtgritman@gmail.com>
2021-11-11 11:40:08 -0500

    Added posts and fixed controllers

--- server/index.js
+++ server/index.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config()
 const usersController = require('./controllers/users');
+const postsController = require('./controllers/posts');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const staticPath = path.join(__dirname, '../docs');
 app
-    .use('/', express.static(path.join(__dirname, '../docs')))
+    .use('/', express.static(staticPath))
+
+    .use(express.json())
     .use('/users', usersController)
-    ;
+    .use('/posts', postsController)
 
 app
     .get('*', (req, res) => res.sendFile(path.join(staticPath, '/index.html')))
-    ;
+
+app
+    .use((err, req, res, next)=>{
+        res.status(err.code || 500).send(err);
+    })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Server listening at http://localhost:${port}`);
+})
