cc191e60e88df126638d104fb4ac2047e6a95354
dtgritman <dtgritman@gmail.com>
2021-11-11 11:40:50 -0500

    Initial user login with server setup

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -6,6 +6,28 @@ const app = express.Router();
 app
     .get('/', (req, res, next) => {
         res.send("test");
-    });
+    })
+    .get("/:userid", (req, res, next) => {
+        res.send(model.get(req.params.userid))
+    })
+    .post("/login", (req, res, next) => {
+        model.login(req.body.handle, req.body.password, (err, user) => {
+            if (err) {
+                next(err);
+            }
+            res.send(user)
+        })
+    })
+    .post("/signup", (req, res, next) => {
+        const user = req.body;
+
+        model.add(user, (err, user) => {
+            if (err) {
+                next(err);
+            }
+            res.send(user)
+        })
+    })
+    ;
 
 module.exports = app;
