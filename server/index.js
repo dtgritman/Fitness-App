f7189302c789e414d080bb16e90210b03b5fa70b
Dustin Gritman <gritmand1@hawkmail.newpaltz.edu>
2021-11-17 11:53:47 -0500

    Added headers

--- server/index.js
+++ server/index.js
@@ -11,6 +11,13 @@ const staticPath = path.join(__dirname, '../docs');
 app
     .use('/', express.static(staticPath))
 
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', '*');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        next();
+    })
+
     .use(express.json())
     .use('/users', usersController)
     .use('/posts', postsController)
