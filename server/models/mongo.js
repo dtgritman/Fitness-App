41018713cb1ff16eda6a81cf8c0aabd8afacd8ad
Dustin Gritman <gritmand1@hawkmail.newpaltz.edu>
2021-11-17 12:30:25 -0500

    Updated mongo to use supplied db name

--- server/models/mongo.js
+++ server/models/mongo.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@fitnessapp.pgox6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@fitnessapp.pgox6.mongodb.net/${processs.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const isConnected = client.connect();
