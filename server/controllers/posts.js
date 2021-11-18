04a7ced643e203864524306e83af29fba947ba72
Dustin Gritman <gritmand1@hawkmail.newpaltz.edu>
2021-11-18 09:48:35 -0500

    mongo setup for posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,20 +5,49 @@ const app = express.Router();
 
 app
     .get("/", (req, res, next) => {
-        res.send(model.getAll());
-    })
-    .get("/search", (req, res, next) => {
-        res.send(model.search(req.query.q));
+        model.getAll()
+            .then(x => res.send(x))
+            .catch(next)
     })
     .get("/:id", (req, res, next) => {
-        console.log(req.headers);
-
-        res.send(model.get(req.params.id));
+        model.get(req.params.id)
+            .then(x => res.send(x))
+            .catch(next)
+    })
+    .get("/wall/:handle", (req, res, next) => {
+        model.getWall(req.params.handle)
+            .then(x => res.send(x))
+            .catch(next)
+    })
+    .get("/feed/:handle", (req, res, next) => {
+        model.getFeed(req.params.handle)
+            .then(x => res.send(x))
+            .catch(next)
     })
     .post("/", (req, res, next) => {
         const newPost = model.add(req.body)
 
         res.status(201).send(newPost);
+    })
+    .post("/", (req, res, next) => {
+        model.add(req.body)
+            .then(x => res.status(201).send(x))
+            .catch(next)
+    })
+    .patch("/:id", (req, res, next) => {
+        model.update(req.params.id, req.body)
+            .then(x => res.send(x))
+            .catch(next)
+    })
+    .delete("/:id", (req, res, next) => {
+        model.remove(req.params.id)
+            .then(x => res.send({ deleted: x }))
+            .catch(next)
+    })
+    .post("/seed", (req, res, next) => {
+        model.seed()
+            .then(x => res.status(201).send("Created"))
+            .catch(next)
     })
 
 module.exports = app;
