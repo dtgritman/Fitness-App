dc36a9a891f3e59fae775d794e7eb2bd3d3bc239
Dustin Gritman <gritmand1@hawkmail.newpaltz.edu>
2021-11-20 12:19:26 -0500

    made moment global

--- client/src/main.js
+++ client/src/main.js
@@ -7,12 +7,18 @@ import fas from '@fortawesome/fontawesome-free-solid/';
 import { Notification, Config } from '@oruga-ui/oruga-next';
 import '@oruga-ui/oruga-next/dist/oruga.css';
 
+import moment from 'moment';
+
 import App from './App.vue';
 import router from './router';
 
 library.add(fas);
-createApp(App)
+
+let app = createApp(App);
+app.config.globalProperties.$moment = moment;
+app
     .use(router)
     .use(Notification)
     .use(Config, { iconPack: 'fas' })
+    .use(moment)
     .mount('#app');
