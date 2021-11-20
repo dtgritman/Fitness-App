07fd250bc9a4178336b051f813e9221c38244f03
Dustin Gritman <gritmand1@hawkmail.newpaltz.edu>
2021-11-20 14:54:45 -0500

    profile editing now uses server

---
+++ client/src/components/ProfileEdit.vue
@@ -0,0 +1,84 @@
+<template>
+    <form
+        class="modal"
+        :class="{ 'is-active': isActive }"
+        @submit.prevent="$emit('close')"
+    >
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">Update Profile</p>
+            </header>
+            <div class="modal-card-body">
+                Sex:
+                <input
+                    class="input"
+                    type="text"
+                    name="sex"
+                    v-model="newProfile.sex"
+                />
+                <br />
+                <br />
+                Birthday:
+                <input
+                    class="input"
+                    type="date"
+                    name="birthday"
+                    v-model="newProfile.birthday"
+                />
+                <br />
+                <br />
+                Height:
+                <input
+                    class="input"
+                    type="number"
+                    name="height"
+                    v-model="newProfile.height"
+                    placeholder="new height in inches(in.)"
+                />in.
+                <br />
+                <br />
+                Weight:
+                <input
+                    class="input"
+                    type="number"
+                    name="weight"
+                    v-model="newProfile.weight"
+                    placeholder="new weight in pounds(lbs.)"
+                />lbs.
+            </div>
+            <footer class="modal-card-foot">
+                <button
+                    class="button is-success"
+                    @click="$emit('update', newProfile)"
+                >
+                    Update
+                </button>
+                <button class="button">Cancel</button>
+            </footer>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    props: {
+        isActive: Boolean,
+        profile: Object,
+    },
+    data() {
+        return {
+            newProfile: {
+                sex: this.profile.sex,
+                birthday: this.$moment(this.profile.birthday).format(
+                    "YYYY-MM-DD"
+                ),
+                height: this.profile.height,
+                weight: this.profile.weight,
+            },
+        };
+    },
+};
+</script>
+
+<style>
+</style>
--- client/src/views/MyProfile.vue
+++ client/src/views/MyProfile.vue
@@ -24,124 +24,69 @@
                         <div class="has-text-centered">
                             <p class="title is-4">Height</p>
                             <p>{{ heightString }}</p>
-                            <a
-                                class="card-footer-item"
-                                @click="modalHeight = true"
-                                >Update
-                            </a>
                         </div>
                     </div>
                     <div class="level-item">
                         <div class="has-text-centered">
                             <p class="title is-4">Weight</p>
                             <p>{{ weightString }}</p>
-                            <a
-                                class="card-footer-item"
-                                @click="modalWeight = true"
-                                >Update
-                            </a>
                         </div>
                     </div>
                 </div>
+                <a class="card-footer-item" @click="editActive = true">Edit</a>
             </div>
         </div>
 
-        <div class="modal" :class="{ 'is-active': modalHeight }">
-            <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-title">Update Height</p>
-                </header>
-                <section class="modal-card-body">
-                    <input
-                        class="input"
-                        type="number"
-                        v-model="newHeight"
-                        placeholder="new height in inches"
-                    />in.
-                </section>
-                <footer class="modal-card-foot">
-                    <button class="button is-success" @click="submitHeight()">
-                        Update
-                    </button>
-                    <button class="button" @click="modalHeight = false">
-                        Cancel
-                    </button>
-                </footer>
-            </div>
-        </div>
-
-        <div
-            id="weightModal"
-            class="modal"
-            :class="{ 'is-active': modalWeight }"
-        >
-            <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-title">Update Weight</p>
-                </header>
-                <section class="modal-card-body">
-                    <input
-                        class="input"
-                        type="number"
-                        v-model="newWeight"
-                        placeholder="new weight in pounds"
-                    />lbs.
-                </section>
-                <footer class="modal-card-foot">
-                    <button class="button is-success" @click="submitWeight()">
-                        Update
-                    </button>
-                    <button class="button" @click="modalWeight = false">
-                        Cancel
-                    </button>
-                </footer>
-            </div>
-        </div>
+        <profile-edit
+            :isActive="editActive"
+            :profile="profile"
+            @update="submitUpdate"
+            @close="editActive = false"
+        />
     </section>
 </template>
 
 <script>
-import moment from "moment";
 import session from "../services/session";
+import { updateProfile } from "../services/users";
+import ProfileEdit from "../components/ProfileEdit.vue";
 
 export default {
     name: "MyProfile",
+    components: { ProfileEdit },
     data: () => ({
-        modalHeight: false,
-        modalWeight: false,
-        newHeight: null,
-        newWeight: null,
+        profile: session.user.profile,
+        editActive: false,
     }),
-    methods: {
-        submitHeight() {
-            session.user.profile.height = this.newHeight;
-            this.modalHeight = false;
-        },
-        submitWeight() {
-            session.user.profile.weight = this.newWeight;
-            this.modalWeight = false;
-        },
-    },
     computed: {
-        sex() {
-            return session.user.profile.sex;
-        },
         name() {
             return session.user.firstName + " " + session.user.lastName;
         },
+        sex() {
+            return this.profile.sex;
+        },
         age() {
-            return moment().diff(moment(session.user.profile.birthday), "years");
+            return this.$moment().diff(this.$moment(this.profile.birthday), "years");
         },
         heightString() {
             return (
-                Math.floor(session.user.profile.height / 12) +
+                Math.floor(this.profile.height / 12) +
                 "' " +
-                (session.user.profile.height % 12) +
+                (this.profile.height % 12) +
                 '"'
             );
         },
         weightString() {
-            return session.user.profile.weight + " lbs.";
+            return this.profile.weight + " lbs.";
+        },
+    },
+    methods: {
+        async submitUpdate(newProfile) {
+            const response = await updateProfile(session.user._id, newProfile);
+            if (response.profile) {
+                this.profile = response.profile;
+                return this.profile;
+            }
         },
     },
 };
