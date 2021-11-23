fe0901494a553a2e7c58afd9f10dffdb604e40a9
Dustin Gritman <gritmand1@hawkmail.newpaltz.edu>
2021-11-23 18:41:24 -0500

    friends api fix and added remove

--- client/src/services/friends.js
+++ client/src/services/friends.js
@@ -1,9 +1,13 @@
 import { api } from "./serverApi";
 
 export function get(handle) {
-    return api("users/friends/" + handle);
+    return api("users/friends/" + handle, {}, 'POST');
 }
 
 export function add(userHandle, followUserHandle) {
     return api("users/friends/add/" + userHandle, followUserHandle, 'POST');
+}
+
+export function remove(userHandle, followUserHandle) {
+    return api("users/friends/remove/" + userHandle, followUserHandle, 'POST');
 }
