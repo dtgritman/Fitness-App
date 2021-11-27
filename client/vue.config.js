3c1f0ad61087ffd26d54af90abfdc5df0f2bb45e
Dustin Gritman <gritmand1@hawkmail.newpaltz.edu>
2021-11-27 12:20:43 -0500

    set title for website

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     outputDir: '../docs',
     publicPath: './',
+    chainWebpack: config => {
+        config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = 'Fit Track'
+            return args
+        })
+    }
 }
