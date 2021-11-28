1d906eb05c08165cc97121de219f66a4a59b635a
Dustin Gritman <gritmand1@hawkmail.newpaltz.edu>
2021-11-28 15:00:11 -0500

    fixed modal so it's scrollable now

--- client/src/components/FriendsFind.vue
+++ client/src/components/FriendsFind.vue
@@ -1,43 +1,49 @@
 <template>
     <div class="modal" :class="{ 'is-active': isActive }">
-        <div class="notification">
-            Users Found:
-            <button class="delete is-medium" @click="$emit('close')">
-                <i class="fas fa-times" />
-            </button>
-            <div v-if="!users.length" class="card">
-                <div class="card-content">
-                    No users found with a handle like "{{ q }}".
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head has-text-centered">
+                <p class="modal-card-title">Users Found</p>
+                <button
+                    class="delete is-large"
+                    @click="$emit('close')"
+                ></button>
+            </header>
+            <section class="modal-card-body">
+                <div v-if="!users.length" class="card">
+                    <div class="card-content">
+                        No users found with a handle like "{{ q }}".
+                    </div>
                 </div>
-            </div>
-            <div class="card" v-for="(user, i) in users" :key="i">
-                <div class="card-content">
-                    <div class="level">
-                        <div class="level-item">
-                            <figure>
-                                <p class="image is-128x128 is-square">
-                                    <img
-                                        :src="user.pic"
-                                        onerror="this.src='/imgs/default-avatar.png'"
-                                    />
+                <div class="card" v-for="(user, i) in users" :key="i">
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-left">
+                                <figure>
+                                    <p class="image is-128x128 is-square">
+                                        <img
+                                            :src="user.pic"
+                                            onerror="this.src='/imgs/default-avatar.png'"
+                                        />
+                                    </p>
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <p class="title is-6">{{ user.handle }}</p>
+                                <p class="subtitle is-6">
+                                    ({{ user.firstName + " " + user.lastName }})
                                 </p>
-                            </figure>
+                            </div>
                         </div>
-                        <div class="level-item">
-                            <p class="title is-6">{{ user.handle }}</p>
-                        </div>
-                        <div class="level-item">
-                            <p>({{ user.firstName + " " + user.lastName }})</p>
-                        </div>
-                        <div
-                            v-if="
-                                friends.findIndex(
-                                    (friend) => friend.handle == user.handle
-                                ) > -1
-                            "
-                            class="level-item"
-                        >
+                    </div>
+                    <div class="card-footer">
+                        <div class="card-footer-item">
                             <button
+                                v-if="
+                                    friends.findIndex(
+                                        (friend) => friend.handle == user.handle
+                                    ) > -1
+                                "
                                 class="button"
                                 @click="
                                     $emit(
@@ -51,15 +57,17 @@
                             >
                                 Remove Friend
                             </button>
-                        </div>
-                        <div v-else class="level-item">
-                            <button class="button" @click="$emit('add', user)">
+                            <button
+                                v-else
+                                class="button"
+                                @click="$emit('add', user)"
+                            >
                                 Add Friend
                             </button>
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
         </div>
     </div>
 </template>
