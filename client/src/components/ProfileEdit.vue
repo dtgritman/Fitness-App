552eed14ae1d0c5c8db9442346d8f3b27ea4a906
Dustin Gritman <gritmand1@hawkmail.newpaltz.edu>
2021-11-28 16:44:56 -0500

    fixed modal layout and form

--- client/src/components/ProfileEdit.vue
+++ client/src/components/ProfileEdit.vue
@@ -1,62 +1,72 @@
 <template>
-    <form
-        class="modal"
-        :class="{ 'is-active': isActive }"
-        @submit.prevent="$emit('close')"
-    >
+    <div class="modal" :class="{ 'is-active': isActive }">
+        <div class="modal-background"></div>
         <div class="modal-card">
-            <header class="modal-card-head">
+            <header class="modal-card-head has-text-centered">
                 <p class="modal-card-title">Update Profile</p>
-            </header>
-            <div class="modal-card-body">
-                Sex:
-                <input
-                    class="input"
-                    type="text"
-                    name="sex"
-                    v-model="newProfile.sex"
-                />
-                <br />
-                <br />
-                Birthday:
-                <input
-                    class="input"
-                    type="date"
-                    name="birthday"
-                    v-model="newProfile.birthday"
-                />
-                <br />
-                <br />
-                Height:
-                <input
-                    class="input"
-                    type="number"
-                    name="height"
-                    v-model="newProfile.height"
-                    placeholder="new height in inches(in.)"
-                />in.
-                <br />
-                <br />
-                Weight:
-                <input
-                    class="input"
-                    type="number"
-                    name="weight"
-                    v-model="newProfile.weight"
-                    placeholder="new weight in pounds(lbs.)"
-                />lbs.
-            </div>
-            <footer class="modal-card-foot">
                 <button
-                    class="button is-success"
-                    @click="$emit('update', newProfile)"
-                >
-                    Update
-                </button>
-                <button class="button">Cancel</button>
+                    class="delete is-large"
+                    @click="$emit('close')"
+                ></button>
+            </header>
+            <form class="modal-card-body" @submit.prevent="$emit('update', newProfile)">
+                <div class="field">
+                    <p class="control">
+                        Sex:
+                        <input
+                            class="input"
+                            type="text"
+                            name="sex"
+                            v-model="newProfile.sex"
+                        />
+                    </p>
+                </div>
+                <div class="field">
+                    <p class="control">
+                        Birthday:
+                        <input
+                            class="input"
+                            type="date"
+                            name="birthday"
+                            v-model="newProfile.birthday"
+                        />
+                    </p>
+                </div>
+                <div class="field">
+                    <p class="control">
+                        Height:
+                        <input
+                            class="input"
+                            type="number"
+                            name="height"
+                            v-model="newProfile.height"
+                            placeholder="new height in inches(in.)"
+                        />in.
+                    </p>
+                </div>
+                <div class="field">
+                    <p class="control">
+                        Weight:
+                        <input
+                            class="input"
+                            type="number"
+                            name="weight"
+                            v-model="newProfile.weight"
+                            placeholder="new weight in pounds(lbs.)"
+                        />lbs.
+                    </p>
+                </div>
+                <div class="field buttons is-centered">
+                    <button class="button is-success">
+                        Update
+                    </button>
+                </div>
+            </form>
+            <footer class="modal-card-foot buttons is-centered">
+                <button class="button" @click="$emit('close')">Cancel</button>
             </footer>
         </div>
-    </form>
+    </div>
 </template>
 
 <script>
@@ -69,9 +79,9 @@ export default {
         return {
             newProfile: {
                 sex: this.profile.sex,
-                birthday: this.profile.birthday ? this.$moment(this.profile.birthday).format(
-                    "YYYY-MM-DD"
-                ) : this.profile.birthday,
+                birthday: this.profile.birthday
+                    ? this.$moment(this.profile.birthday).format("YYYY-MM-DD")
+                    : this.profile.birthday,
                 height: this.profile.height,
                 weight: this.profile.weight,
             },
