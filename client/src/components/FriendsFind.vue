9a6d5f30bb500e2cc3258508aa739f275be1281f
Dustin Gritman <gritmand1@hawkmail.newpaltz.edu>
2021-11-28 14:00:02 -0500

    adjusted layout some and added user pic and name

--- client/src/components/FriendsFind.vue
+++ client/src/components/FriendsFind.vue
@@ -12,19 +12,47 @@
             </div>
             <div class="card" v-for="(user, i) in users" :key="i">
                 <div class="card-content">
-                    <div class="level is-mobile">
+                    <div class="level">
                         <div class="level-item">
-                            <p class="title is-6">User Handle:</p>
-                            <p>{{ user.handle }}</p>
+                            <figure class="image is-64x64">
+                                <img
+                                    :src="user.pic"
+                                    onerror="this.src='/imgs/default-avatar.png'"
+                                />
+                            </figure>
                         </div>
-                        <div v-if="friends.findIndex(friend => friend.handle == user.handle) > -1" class="level-item">
-                            <button class="button" @click="$emit('remove', friends.findIndex(friend => friend.handle == user.handle))">
-                                <i class="fas fa-minus" />
+                        <div class="level-item">
+                            <p class="title is-6">{{ user.handle }}</p>
+                        </div>
+                        <div class="level-item">
+                            <p>({{ user.firstName + " " + user.lastName }})</p>
+                        </div>
+                        <div
+                            v-if="
+                                friends.findIndex(
+                                    (friend) => friend.handle == user.handle
+                                ) > -1
+                            "
+                            class="level-item"
+                        >
+                            <button
+                                class="button"
+                                @click="
+                                    $emit(
+                                        'remove',
+                                        friends.findIndex(
+                                            (friend) =>
+                                                friend.handle == user.handle
+                                        )
+                                    )
+                                "
+                            >
+                                Remove Friend
                             </button>
                         </div>
                         <div v-else class="level-item">
                             <button class="button" @click="$emit('add', user)">
-                                <i class="fas fa-plus" />
+                                Add Friend
                             </button>
                         </div>
                     </div>
