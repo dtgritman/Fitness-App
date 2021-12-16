794a8dbaeaac24bf08497acb4e1ca56047c2b238
dtgritman <dtgritman@gmail.com>
2021-12-16 13:24:24 -0500

    removed oruga autocomplete

--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,7 @@ import moment from 'moment';
 import { library } from '@fortawesome/fontawesome';
 import fas from '@fortawesome/fontawesome-free-solid/';
 
-import { Autocomplete, Notification, Config } from '@oruga-ui/oruga-next';
+import { Notification, Config } from '@oruga-ui/oruga-next';
 import '@oruga-ui/oruga-next/dist/oruga.css';
 
 import App from './App.vue';
@@ -17,7 +17,6 @@ const app = createApp(App);
 app.config.globalProperties.$moment = moment;
 app
     .use(router)
-    .use(Autocomplete)
     .use(Notification)
     .use(Config, { iconPack: 'fas' })
     .mount('#app');
